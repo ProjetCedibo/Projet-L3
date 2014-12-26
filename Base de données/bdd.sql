f6aee237bd89d6efabbbb4c4e7c0e94decb673e9
kingramizy <lesniperdu25@free.fr>
2014-12-26 02:22:25 +0100

    appli
    appi

--- Base de données/bdd.sql
+++ Base de données/bdd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:8889
--- Généré le :  Ven 26 Décembre 2014 à 01:00
+-- Généré le :  Ven 26 Décembre 2014 à 02:21
 -- Version du serveur :  5.5.38
 -- Version de PHP :  5.5.18
 
@@ -13,6 +13,45 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `UFR_SJEPG`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Admin`
+--
+
+CREATE TABLE `Admin` (
+`AdminId` int(11) NOT NULL,
+  `AdminPseudo` varchar(50) CHARACTER SET utf32 NOT NULL,
+  `AdminPassWord` varchar(50) CHARACTER SET utf16 NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `Admin`
+--
+
+INSERT INTO `Admin` (`AdminId`, `AdminPseudo`, `AdminPassWord`) VALUES
+(1, 'Admin', '4e7afebcfbae000b22c7c85e5560f89a2a0280b4');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Connection`
+--
+
+CREATE TABLE `Connection` (
+`ConnectionId` int(11) NOT NULL,
+  `ConnectionDate` date DEFAULT NULL,
+  `ConnectionHour` time DEFAULT NULL,
+  `ConnectionUser` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `Connection`
+--
+
+INSERT INTO `Connection` (`ConnectionId`, `ConnectionDate`, `ConnectionHour`, `ConnectionUser`) VALUES
+(1, '2014-12-03', '11:12:13', 1);
 
 -- --------------------------------------------------------
 
@@ -38,6 +77,18 @@ INSERT INTO `User` (`UserId`, `UserDevice`, `UserLogin`) VALUES
 --
 
 --
+-- Index pour la table `Admin`
+--
+ALTER TABLE `Admin`
+ ADD PRIMARY KEY (`AdminId`);
+
+--
+-- Index pour la table `Connection`
+--
+ALTER TABLE `Connection`
+ ADD PRIMARY KEY (`ConnectionId`);
+
+--
 -- Index pour la table `User`
 --
 ALTER TABLE `User`
@@ -47,6 +98,16 @@ ALTER TABLE `User`
 -- AUTO_INCREMENT pour les tables exportées
 --
 
+--
+-- AUTO_INCREMENT pour la table `Admin`
+--
+ALTER TABLE `Admin`
+MODIFY `AdminId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `Connection`
+--
+ALTER TABLE `Connection`
+MODIFY `ConnectionId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `User`
 --
---
+++ appli/appli/appli/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appli
+//
+//  Created by petetin cédric on 26/12/2014.
+//  Copyright (c) 2014 Petetin Cédric & Akrach Ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

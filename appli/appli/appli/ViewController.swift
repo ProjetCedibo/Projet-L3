d7d091cb8833dd6aa82cde3654da64e721772369
Erolyam <obibim@hotmail.fr>
2014-12-29 01:09:55 +0100

    end

--- appli/appli/appli/ViewController.swift
+++ appli/appli/appli/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var Test: UIButton!
+    
 
+    @IBOutlet var bar: UIView!
 }
 

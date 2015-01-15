5bff65a8c38233f1b8b9ce5bb21c7ccab4f763a4
kingramizy <lesniperdu25@free.fr>
2015-01-15 11:01:36 +0100

    15/01 11:01

---
+++ appli/SJEPG/SJEPG/LeftMenuView.swift
@@ -0,0 +1,13 @@
+//
+//  LeftMenuView.swift
+//  SJEPG
+//
+//  Created by petetin cédric on 15/01/2015.
+//  Copyright (c) 2015 Petetin Cédric & Akrach Ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class LeftMenuView: UIViewController {
+
+}

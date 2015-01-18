91fc1d774f5b53e093ce7f0c1554fd358960e0d3
kingramizy <lesniperdu25@free.fr>
2015-01-18 13:41:31 +0100

    appli

---
+++ appli/SJEPG/SJEPG/ContainerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ContainerViewController.swift
+//  SJEPG
+//
+//  Created by petetin cédric on 18/01/2015.
+//  Copyright (c) 2015 Petetin Cédric & Akrach Ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+    var centerNavigationController: UINavigationController!
+    var centerViewController: ViewController!
+    
+    
+}
